375a1434f15f7e1adf2d319beb1080fb63886b0d
Luis Felipe <luis.felipeszsv@gmail.com>
2024-08-16 18:10:25 -0300

    improve code

--- app/src/main/java/com/example/pomodoro/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro/presentation/MainActivity.kt
@@ -1,9 +1,3 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
- * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
- * changes to the libraries and their usages.
- */
-
 package com.example.pomodoro.presentation
 
 import android.os.Bundle
@@ -11,45 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import com.example.pomodoro.R
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.*
+import com.example.pomodoro.R
 import com.example.pomodoro.presentation.theme.PomodoroTheme
 import kotlinx.coroutines.delay
-import androidx.compose.runtime.*
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.Button
-import androidx.wear.compose.material.ButtonDefaults
-import androidx.wear.compose.material.CircularProgressIndicator
-import androidx.wear.compose.material.Icon
-import androidx.wear.compose.material.OutlinedButton
-import androidx.wear.compose.material.ProgressIndicatorDefaults
 import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Install splash screen
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
 
+        // Set the theme for the activity
         setTheme(android.R.style.Theme_DeviceDefault)
 
+        // Set the content view with Jetpack Compose
         setContent {
             WearApp()
         }
@@ -74,66 +55,88 @@ fun WearApp() {
 fun Counter() {
     val startTime = 5 * 60
     var timeLeft by remember { mutableIntStateOf(startTime) }
+    var isRunning by remember { mutableStateOf(false) }
 
-    LaunchedEffect(key1 = timeLeft) {
-        if (timeLeft > 0) {
-            delay(1000L)
-            timeLeft -= 1
+    // Function to pause the timer
+    fun playPauseTimer() {
+        isRunning = !isRunning
+    }
+
+    // Function to reset the timer
+    fun resetTimer() {
+        timeLeft = startTime
+        isRunning = false
+    }
+
+    // Timer countdown logic
+    LaunchedEffect(isRunning) {
+        if (isRunning && timeLeft > 0) {
+            while (isRunning && timeLeft > 0) {
+                delay(1000L)
+                timeLeft -= 1
+            }
         }
     }
 
-    val minutes = (timeLeft / 60)
-    val seconds = (timeLeft % 60)
+    // Format time as MM:SS
+    val minutes = timeLeft / 60
+    val seconds = timeLeft % 60
     val timeFormatted = String.format(Locale.ENGLISH, "%02d:%02d", minutes, seconds)
 
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = timeFormatted
-    )
-
-    CircularProgressIndicator(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(all = 1.dp),
-        startAngle = 270f,
-        progress = timeLeft.toFloat() / startTime.toFloat(),
-        strokeWidth = ProgressIndicatorDefaults.FullScreenStrokeWidth
-    )
-
-    Row(verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.spacedBy(space = 4.dp,alignment = Alignment.CenterHorizontally ),
-        modifier = Modifier.fillMaxSize()
-    ) {
-        OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.size(ButtonDefaults.SmallButtonSize)) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_play_arrow_30),
-                contentDescription = "airplane",
-                modifier = Modifier
-                    .size(ButtonDefaults.LargeIconSize).wrapContentSize(align = Alignment.Center),
-            )
-        }
-
-    Button(onClick = { /*TODO*/ }, modifier = Modifier.size(ButtonDefaults.SmallButtonSize)) {
-
-        Icon(
-            painter = painterResource(id = R.drawable.baseline_pause_30),
-            contentDescription = "airplane",
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
             modifier = Modifier
-                .size(ButtonDefaults.LargeIconSize).wrapContentSize(align = Alignment.Center),
+                .fillMaxSize()
+                .padding(4.dp),
+            startAngle = 270f,
+            progress = timeLeft / startTime.toFloat(),
+            strokeWidth = ProgressIndicatorDefaults.FullScreenStrokeWidth
         )
-    }
 
+        Text(
+            text = timeFormatted,
+            style = MaterialTheme.typography.display1,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier.align(Alignment.Center)
+        )
 
+        Row(
+
+                    verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.spacedBy(space = 4.dp,alignment = Alignment.CenterHorizontally ),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            OutlinedButton(
+                onClick = { resetTimer() },
+                modifier = Modifier.size(ButtonDefaults.SmallButtonSize)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_stop_30),
+                    contentDescription = "Start",
+                    modifier = Modifier.size(ButtonDefaults.LargeIconSize)
+                )
+            }
+
+            Button(
+                onClick = { playPauseTimer() },
+                modifier = Modifier.size(ButtonDefaults.SmallButtonSize)
+            ) {
+                val iconId = if (isRunning) R.drawable.baseline_pause_30 else R.drawable.baseline_play_arrow_30
+                val contentDescription = if (isRunning) "Pause" else "Play"
+
+                Icon(
+                    painter = painterResource(id = iconId),
+                    contentDescription,
+                    modifier = Modifier.size(ButtonDefaults.LargeIconSize)
+                )
+            }
+        }
     }
 }
-
 
 @Preview(device = Devices.WEAR_OS_SQUARE, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     WearApp()
 }
-
-
