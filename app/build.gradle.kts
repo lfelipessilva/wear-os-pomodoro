cef02fec8a51d7dfabdaa180df2b00620bdf1e4c
Luis Felipe <luis.felipeszsv@gmail.com>
2024-08-22 14:58:14 -0300

    release: 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.avec.pomodoro"
         minSdk = 33
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
         vectorDrawables {
             useSupportLibrary = true
         }
