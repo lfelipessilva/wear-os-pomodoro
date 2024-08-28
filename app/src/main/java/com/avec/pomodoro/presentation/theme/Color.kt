5fcd9d13590f346ee86fd7831dd0703b088693a9
Luis Felipe <luis.felipeszsv@gmail.com>
2024-08-28 01:36:51 -0300

    refactor: change red color

--- app/src/main/java/com/avec/pomodoro/presentation/theme/Color.kt
+++ app/src/main/java/com/avec/pomodoro/presentation/theme/Color.kt
@@ -2,5 +2,5 @@ package com.avec.pomodoro.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Red = Color(0xFF8B0000) // Base red color
+val Red = Color(0xFFB31B1B) // Base red color
 val Gray = Color(0xFF5B5859) // Gray color
